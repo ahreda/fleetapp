c71075425d4210b7ca20853aef317af66a345834
Ahmad Reda <ahmad.ah.reda@gmail.com>
2020-08-04 14:38:31 +0200

    welcome message

--- src/main/java/com/spring/fleetapp/FleetappApplication.java
+++ src/main/java/com/spring/fleetapp/FleetappApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FleetappApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FleetappApplication.class, args);
+		System.out.println("Hello to Fleet Management Application");
 	}
 
 }
